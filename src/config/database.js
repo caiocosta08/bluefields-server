c30e7699980acc68e4ed2c1b9fe9d4cb6f1151e9
Igor Sales <igorsales.fs@gmail.com>
2021-03-24 19:50:33 -0300

    config dotenv

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
-  "username": "root",
-  "password": null, //null
-  "database": "bihands_db",
-  "host": "127.0.0.1",
+  "username": process.env.DB_USER,
+  "password": process.env.DB_PASS,
+  "database":  process.env.DB_DATABASE,
+  "host":  process.env.DB_HOST,
   // "port": 8889, //excluir
   "dialect": "mysql",
   "define": {
