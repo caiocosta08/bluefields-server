05211eb720cc4a98b4a4e0fc0ea0163337ead476
Igor Sales <igorsales.fs@gmail.com>
2021-03-25 16:21:24 -0300

    add model Product and Category

--- src/database/index.js
+++ src/database/index.js
@@ -3,13 +3,17 @@ import Sequelize from 'sequelize';
 import User from '../app/models/User';
 import Shop from '../app/models/Shop';
 import Address from '../app/models/Address';
+import Category from '../app/models/Category';
+import Product from '../app/models/Product';
 
 import databaseConfig from '../config/database';
 
 const models = [
    User,
    Shop,
-   Address, 
+   Address,
+   Category, 
+   Product
 ]
 
 class Database{
