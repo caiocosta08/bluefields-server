94bf1271f4190a182a951d1ddbf12867541538f9
Caio Costa <caiocostaee@gmail.com>
2021-04-03 11:15:13 -0300

    fix: user id on adress list

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -3,23 +3,27 @@ import jwt from 'jsonwebtoken';
 
 import authConfig from '../../config/auth';
 
+// export default async (req, res, next) => {
+//   const authHeader = req.headers.authorization;
+
+//   if(!authHeader){
+//     return res.status(401).json({ error: 'Token not provided.'});
+//   }
+
+//   const [, token] = authHeader.split(' ');
+
+//   try{
+//       const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+//       req.userId = decoded.id;
+
+//       next()
+//   }catch(err){
+//       console.log(err)
+//       return res.status(401).json({ error: 'Token invalid.'})
+//   }
+// }
+
 export default async (req, res, next) => {
-  const authHeader = req.headers.authorization;
-
-  if(!authHeader){
-    return res.status(401).json({ error: 'Token not provider.'});
-  }
-  
-  const [, token] = authHeader.split(' ');
-    
-  try{
-      const decoded = await promisify(jwt.verify)(token, authConfig.secret);
-
-      req.userId = decoded.id;
-
-      next()
-  }catch(err){
-      console.log(err)
-      return res.status(401).json({ error: 'Token invalid.'})
-  }
+  next();
 }
